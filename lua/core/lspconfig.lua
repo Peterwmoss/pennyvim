ca397b47af8db3b85892e1d3ebab940d73e3d94c
Peter K. Moss <peter@moss.dk>
2021-07-25 16:48:17 +0200

    LSP work

--- lua/core/lspconfig.lua
+++ lua/core/lspconfig.lua
@@ -5,3 +5,45 @@ local servers = { "tsserver", "ccls", "html", "texlab" }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup{}
 end
+
+local utils = require "utils"
+
+local options = {
+  silent = true,
+  noremap = true,
+}
+
+local keybinds = {
+  { "K", ":lua vim.lsp.buf.hover()<CR>" },
+}
+
+utils.add_keymap_normal(options, keybinds)
+
+-- symbols for autocomplete
+vim.lsp.protocol.CompletionItemKind = {
+  "   (Text) ",
+  "   (Method)",
+  "   (Function)",
+  "   (Constructor)",
+  " ﴲ  (Field)",
+  "[] (Variable)",
+  "   (Class)",
+  " ﰮ  (Interface)",
+  "   (Module)",
+  " 襁 (Property)",
+  "   (Unit)",
+  "   (Value)",
+  " 練 (Enum)",
+  "   (Keyword)",
+  "   (Snippet)",
+  "   (Color)",
+  "   (File)",
+  "   (Reference)",
+  "   (Folder)",
+  "   (EnumMember)",
+  " ﲀ  (Constant)",
+  " ﳤ  (Struct)",
+  "   (Event)",
+  "   (Operator)",
+  "   (TypeParameter)",
+}
