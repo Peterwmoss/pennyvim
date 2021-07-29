062c1c1443930ece77ecd71b9681b7154f8b7af9
Peter K. Moss <peter@moss.dk>
2021-07-29 15:10:36 +0200

    Updated example config to contain right notion of custom keymaps

--- utils/config.example.lua
+++ utils/config.example.lua
@@ -11,8 +11,9 @@ pvim.custom_plugins = {}
   -- All supported LSP servers can be found here: https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md
   -- LSP servers that can be installed with :LspInstall <lang> can be found here: https://github.com/kabouzeid/nvim-lspinstall#bundled-installers
   -- lsp_servers = {},
+
 pvim.custom_mappings = {
-  -- { "<mode>", "<key combination>", "<command>", options },
+  -- { mode = "<mode>", comb = "<key combination>", mapping = "<mapping>", options = <option> },
   -- options have the format: { silent = <boolean>, expr = <boolean>, noremap = <boolean> }
   -- Mode have the format: "n" (normal), "i" (insert), "v" (visual), "x" (visual block)
 }
