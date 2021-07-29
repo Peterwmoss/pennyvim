a492f57d477e75f5f68461bfe3c0b565b7bc98de
Peter K. Moss <peter@moss.dk>
2021-07-29 19:46:24 +0200

    Removed unnessesary code from plugin loader

--- lua/plugin-loader.lua
+++ lua/plugin-loader.lua
@@ -1,28 +1,12 @@
--- Mostly taken from https://github.com/ChristianChiarulli/LunarVim/blob/c1b0c6f065591fb2259f37d35cd11dbded86edf9/lua/plugin-loader.lua
-
 local plugin_loader = {}
 
 function plugin_loader:init()
-  local execute = vim.api.nvim_command
-
-  execute "packadd packer.nvim"
-
-  local packer_ok, packer = pcall(require, "packer")
-  if not packer_ok then
-    return
-  end
+  vim.api.nvim_command "packadd packer.nvim"
 
   local util = require "packer.util"
-
   packer.init {
     package_root = util.join_paths "~/.local/share/pennyvim/site/pack/",
     compile_path = util.join_paths("~/.config/pvim", "plugin", "packer_compiled.lua"),
-    git = { clone_timeout = 300 },
-    display = {
-      open_fn = function()
-        return util.float { border = "single" }
-      end,
-    },
   }
 
   self.packer = packer
