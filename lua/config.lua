7a748d927eaa021ed5f7321eed57876402d26d48
Peter K. Moss <peter@moss.dk>
2021-07-29 17:35:08 +0200

    accidental remove of used parameter

--- lua/config.lua
+++ lua/config.lua
@@ -1,6 +1,7 @@
 pvim = {
   leader = "space",
   colorscheme = "edge",
+  plugins = {},
 
   -- If user decides to remove this from their own config, we need to ensure that it doesn't fail.
   custom_plugins = {},
