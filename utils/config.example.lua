f4bce2b259f336fa392254ea4e5d30b3dd16c663
Peter K. Moss <peter@moss.dk>
2021-07-29 19:33:10 +0200

    Example config updated with working custom plugins and no custom lsp install

--- utils/config.example.lua
+++ utils/config.example.lua
@@ -4,13 +4,8 @@ pvim.leader = "space"
   -- Your preferred colorscheme, this one, among others, has treesitter support
 pvim.colorscheme = "edge"
 
-  -- TODO Make this work
   -- Add custom plugins
 pvim.custom_plugins = {}
-
-  -- All supported LSP servers can be found here: https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md
-  -- LSP servers that can be installed with :LspInstall <lang> can be found here: https://github.com/kabouzeid/nvim-lspinstall#bundled-installers
-  -- lsp_servers = {},
 
 pvim.custom_mappings = {
   -- { mode = "<mode>", comb = "<key combination>", mapping = "<mapping>", options = <option> },
